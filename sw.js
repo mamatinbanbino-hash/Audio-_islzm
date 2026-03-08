a95ac80c0a702b305ea5c1318f391b6ccc4a78f4
mamatinbanbino-hash <mamatinbanbino@gmail.com>
2026-03-08 14:31:10 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,27 @@
-// Contenu du fichier sw.js
-const CACHE_NAME = 'audios-islam-v1';
+// Contenu optimisé du fichier sw.js
+const CACHE_NAME = 'audios-islam-v2'; // Changé en v2 car on modifie la liste
 const urlsToCache = [
   '/',
   '/index.html',
-  '/audios-islamique-icon.png'
+  '/audios-islamique-icon.png',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Cache ouvert');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      // Retourne le fichier du cache s'il existe, sinon fait une requête réseau
+      return response || fetch(event.request);
+    })
   );
 });
